0265f650d48cc7fe9193d905e84d167aba5ef9e4
osama <osamau3@yahoo.com>
2018-02-21 16:44:55 +0200

    service
    service with model

--- src/app/shared/recipe.service.ts
+++ src/app/shared/recipe.service.ts
@@ -1,18 +1,28 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
+import { Ingredient } from './ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A test recipe 1', 'Description test 1', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
-    new Recipe('A test recipe 2', 'Description test 2', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+    new Recipe('A test recipe 1', 'Description test 1', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
+      [
+        new Ingredient('fish', 1),
+        new Ingredient('beard', 5)
+      ]),
+    new Recipe('A test recipe 2', 'Description test 2', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
+      [
+        new Ingredient('meat', 2),
+        new Ingredient('fresh firze', 12)
+      ]
+    )
   ];
-  
+
   constructor() { }
-  getRecipes(){
+  getRecipes() {
     return this.recipes.slice();
   }
-  getselectedRecipe(recipe : Recipe){
+  getselectedRecipe(recipe: Recipe) {
     return this.recipeSelected.emit(recipe);
   }
 }
